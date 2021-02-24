ba3da6e0ed692fdd36510dae4d9f667303f7b6b0
Saurabh Gangwar <saurabhgangwar621@gmail.com>
2021-02-25 02:02:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 const jwt = require('jsonwebtoken')
-const JWT_SECRET="ryrgwkejtowhfjhsbflew"
+
 var bodyParser = require('body-parser')
 
 
