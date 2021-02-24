a27b86a84bdea89aab92162a1625ad057602c812
Saurabh Gangwar <saurabhgangwar621@gmail.com>
2021-02-25 01:59:53 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OTPbox from './components/Otpbox';
+
+function App() {
+  return (
+    <div className="App">
+        <OTPbox />
+    </div>
+  );
+}
+
+export default App;
